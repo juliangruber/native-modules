18794809f08be4c8c61c374307a8be44f513516e
Julian Gruber <julian@juliangruber.com>
2021-08-12 09:40:17 +0200

    modernize

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
 #!/usr/bin/env node
 
-'use strict'
+import { promises as fs } from 'fs'
+import isNative from 'is-native-module'
+import chalk from 'chalk'
 
-const fs = require('fs')
-const isNative = require('is-native-module')
-const chalk = require('chalk')
-
-let first = true
+let isFirst = true
 
 const check = truthy =>
   truthy
     ? chalk.green('✓')
     : chalk.red('×')
 
-const scan = dir => {
-  fs.readdir(dir, (err, files) => {
-    if (err) return
-    fs.readFile(`${dir}/package.json`, (err, json) => {
-      if (err) return
-      const pkg = JSON.parse(json.toString('utf8'))
-      if (isNative(pkg)) {
-        if (first) {
-          console.log('PB CI Module')
-          first = false
-        }
-        const path = dir
-          .replace('node_modules/', '')
-          .replace(/\/?node_modules\//g, ' -> ')
-        const pb = pkg.dependencies && !!pkg.dependencies.prebuild
-        const ci = pkg.devDependencies && !!pkg.devDependencies['prebuild-ci']
-        console.log(`${check(pb)}  ${check(ci)}  ${path}`)
-      }
-    })
-    files
+const scan = dir => Promise.all([
+  (async () => {
+    let files
+    try {
+      files = await fs.readdir(dir)
+    } catch (_) {
+      return
+    }
+    await Promise.all(files
       .filter(f => !/^\./.test(f))
-      .forEach(f => scan(`${dir}/${f}`))
-  })
-}
+      .map(f => scan(`${dir}/${f}`)))
+  })(),
+  (async () => {
+    let json
+    try {
+      json = await fs.readFile(`${dir}/package.json`)
+    } catch (_) {
+      return
+    }
+    const pkg = JSON.parse(json.toString('utf8'))
+    if (isNative(pkg)) {
+      if (isFirst) {
+        console.log('PB CI Module')
+        isFirst = false
+      }
+      const path = dir
+        .replace('node_modules/', '')
+        .replace(/\/?node_modules\//g, ' -> ')
+      const pb = pkg.dependencies && !!pkg.dependencies.prebuild
+      const ci = pkg.devDependencies && !!pkg.devDependencies['prebuild-ci']
+      console.log(`${check(pb)}  ${check(ci)}  ${path}`)
+    }
+  })()
+])
 
-scan('node_modules')
+scan('node_modules').catch(err => {
+  console.error(err)
+  process.exit(1)
+})
 
